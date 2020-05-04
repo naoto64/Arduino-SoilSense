c0708733156a260614d0d1f4becafd4474ff3104
naoto64 <51907267+naoto64@users.noreply.github.com>
2020-05-04 19:31:14 +0900

    Update SoilSense.cpp

--- src/SoilSense.cpp
+++ src/SoilSense.cpp
@@ -20,10 +20,10 @@ int SoilSense::measure() {
 byte SoilSense::value(int min, int max) {
   _min = min;
   _max = max;
-  int val = measure();
-  val -= _min;
-  if(val < 0) val = 0;
-  val = (byte)(val * 100 / (_max - _min));
+  int rawVal = measure();
+  rawVal -= _min;
+  if(rawVal < 0) rawVal = 0;
+  byte val = (byte)(rawVal * 100 / (_max - _min));
   if(val > 100) val = 100;
   return val;
 }
