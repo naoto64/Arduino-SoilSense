60bfcf9e07cf69251c8628e97d7e9ed493757b56
naoto64 <51907267+naoto64@users.noreply.github.com>
2020-05-04 22:09:02 +0900

    Update SoilSense.h

--- src/SoilSense.h
+++ src/SoilSense.h
@@ -8,12 +8,15 @@
 class SoilSense {
 public:
   SoilSense(uint8_t pin);
+  init(int min, int max, byte lowpass);
   int measure();
-  byte value(int min, int max);
+  byte value();
 
 private:
   long convert(int value);
   uint8_t _pin;
+  byte _lowpass;
+  int _value;
   long _min;
   long _max;
 };
